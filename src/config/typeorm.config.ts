3ebe0e008e195b90e3d42506d767c04f92f3929d
jeton <kreatx23>
2020-08-04 12:20:56 +0200

    Configuration Heroku Postgres database testing.

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,11 +5,17 @@ const dbConfig = config.get('db');
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: dbConfig.type,
-  host: process.env.RDS_HOSTNAME || dbConfig.host,
-  port: process.env.RDS_PORT || dbConfig.port,
-  username: process.env.RDS_USERNAME || dbConfig.username,
-  password: process.env.RDS_PASSWORD || dbConfig.password,
-  database: process.env.RDS_DB_NAME || dbConfig.database,
+  // host: process.env.RDS_HOSTNAME || dbConfig.host,
+  host: dbConfig.host,
+  // port: process.env.RDS_PORT || dbConfig.port,
+  port: dbConfig.port,
+  // username: process.env.RDS_USERNAME || dbConfig.username,
+  username: dbConfig.username,
+  // password: process.env.RDS_PASSWORD || dbConfig.password,
+  password: dbConfig.password,
+  // database: process.env.RDS_DB_NAME || dbConfig.database,
+  database: dbConfig.database,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize,
+  // synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize,
+  synchronize: dbConfig.synchronize,
 }
