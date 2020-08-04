e598ef1c343e972ff897c731c7f1e6506cc194f1
jeton <kreatx23>
2020-08-04 22:01:32 +0200

    Testing Heroku database connection.

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,28 +3,14 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 const dbConfig = config.get('db');
 
-console.log(`type: ${dbConfig.type}`);
-console.log(`host: ${dbConfig.host}`);
-console.log(`port: ${dbConfig.port}`);
-console.log(`username: ${dbConfig.username}`);
-console.log(`password: ${dbConfig.password}`);
-console.log(`database: ${dbConfig.database}`);
-console.log(`synchronize: ${process.env.TYPEORM_SYNC || dbConfig.synchronize}`);
-
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: dbConfig.type,
-  // host: process.env.RDS_HOSTNAME || dbConfig.host,
-  host: dbConfig.host,
-  // port: process.env.RDS_PORT || dbConfig.port,
-  port: dbConfig.port,
-  // username: process.env.RDS_USERNAME || dbConfig.username,
-  username: dbConfig.username,
-  // password: process.env.RDS_PASSWORD || dbConfig.password,
-  password: dbConfig.password,
-  // database: process.env.RDS_DB_NAME || dbConfig.database,
-  database: dbConfig.database,
+  host: process.env.RDS_HOSTNAME || dbConfig.host,
+  port: process.env.RDS_PORT || dbConfig.port,
+  username: process.env.RDS_USERNAME || dbConfig.username,
+  password: process.env.RDS_PASSWORD || dbConfig.password,
+  database: process.env.RDS_DB_NAME || dbConfig.database,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize,
-  // synchronize: dbConfig.synchronize,
   ssl: true
 }
